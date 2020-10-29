a4b5f6b280213a8a66fea006bba5d5714a2da15c
Nikolay Kiselev <ceo@machineand.me>
2020-10-29 23:01:06 +0300

    High load server, background collection, gatling tests

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/machineandme/ticker-backend-go
+
+go 1.15
+
+require github.com/sirupsen/logrus v1.7.0 // indirect
--- main.go
+++ main.go
@@ -1,73 +1,144 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"fmt"
-	"math/rand"
+	"flag"
+	log "github.com/sirupsen/logrus"
 	"net"
-	"os"
 	"strings"
+	"bytes"
+	"sync"
 	"time"
 )
 
-var text = []byte(`HTTP/1.1 200 OK
-Server: High Availability Counter
-Accept-Ranges: bytes
-Content-Length: 2
-Connection: close
+var lock sync.Mutex
+
+var ok = []byte(`HTTP/1.1 200 OK
+Server: HA-Counter (github.com/kiselev-nikolay/ha-counter)
+Connection: keep-alive
+Cache-Control: no-store
+Access-Control-Allow-Origin: *
 Content-Type: text/plain
+Content-Length: 2
 
 ok`)
 
-func main() {
-	arguments := os.Args
-	if len(arguments) == 1 {
-		fmt.Println("Please provide a port number!")
+var fail = []byte(`HTTP/1.1 503 Service Unavailable
+Server: HA-Counter (github.com/kiselev-nikolay/ha-counter)
+Connection: keep-alive
+Cache-Control: no-store
+Access-Control-Allow-Origin: *
+Content-Type: text/plain
+Content-Length: 4
+
+fail`)
+
+type Event struct {
+	Type string
+	ID   string
+}
+
+var count map[Event]bool
+var timer map[Event]int64
+
+func coldStorage() {
+	for {
+		t := time.Now().Unix()
+		lock.Lock()
+		countCopy := count
+		count = make(map[Event]bool)
+		lock.Unlock()
+		for k, _ := range countCopy {
+			timer[k] = t
+		}
+		// log.Debug(timer)
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func processConnection(connection net.Conn, ctx context.Context) {
+	defer func() {
+		defer func() {
+			recover()
+			return
+		}()
+		recover()
+		connection.Write(fail)
+		connection.Close()
+		return
+	}()
+	select {
+	default:
+		request := make([]byte, 255)
+		_, err := connection.Read(request)
+		if err != nil {
+			connection.Write(fail)
+			connection.Close()
+			return
+		}
+		lineEnd := bytes.IndexAny(request, "\n")
+		netData := string(request[:lineEnd])
+		path := strings.Fields(netData)[1]
+		path = strings.Trim(path, "/")
+		pathParts := strings.Split(path, "/")
+		lock.Lock()
+		defer lock.Unlock()
+		e := Event{
+			Type: pathParts[0],
+			ID:   pathParts[1],
+		}
+		// log.Debug(e)
+		count[e] = true
+		connection.Write(ok)
+		connection.Close()
+		return
+	case <-ctx.Done():
+		connection.Write(fail)
+		connection.Close()
 		return
 	}
+}
 
-	PORT := ":" + arguments[1]
-	l, err := net.Listen("tcp4", PORT)
+func main() {
+	host := flag.String("host", "127.0.0.1", "Hostname binding")
+	port := flag.String("port", "8078", "TCP port binding")
+	isJson := flag.Bool("json", false, "Use only json logging")
+	isVerbose := flag.Bool("v", false, "Use verbose logging")
+	flag.Parse()
+
+	if *isJson {
+		log.SetFormatter(&log.JSONFormatter{})
+	}
+
+	if *isVerbose {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	log.WithFields(log.Fields{
+		"host": *host,
+		"port": *port,
+	}).Info("Starting server")
+
+	count = make(map[Event]bool)
+	timer = make(map[Event]int64)
+
+	l, err := net.Listen("tcp", *host+":"+*port)
 	if err != nil {
-		fmt.Println(err)
+		log.Error(err)
 		return
 	}
 	defer l.Close()
 
-	rand.Seed(time.Now().Unix())
+	go coldStorage()
 
 	for {
-		c, err := l.Accept()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer c.Close()
-
 		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
+		connection, _ := l.Accept()
+		// ip := connection.RemoteAddr()
+		go processConnection(connection, ctx)
 		go func() {
-			select {
-			default:
-				buff := bufio.NewReader(c)
-				netData, err := buff.ReadString('\n')
-				if err != nil {
-					return
-				}
-				request := strings.Fields(netData)
-				fmt.Println(request[1])
-				c.Write(text)
-				return
-			case <-ctx.Done():
-				return
-			}
-		}()
-
-		go func() {
-			<-time.After(500 * time.Millisecond)
-			return
+			<-time.After(2 * time.Second)
+			cancel()
 		}()
 	}
 }
